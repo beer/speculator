4b950db88c34067c03532563e6e8c6b5421c599a
beer <beer@eatgo.com>
2013-02-03 23:13:34 +0800

    update for hisoku Signed-off-by: beer <beer@eatgo.com>

--- webdata/models/SpeculatorTable.php
+++ webdata/models/SpeculatorTable.php
@@ -3,10 +3,18 @@ class SpeculatorTable extends Pix_Table
 {
     protected function _getDb() 
     {
-        $json = file_get_contents(__DIR__ . '/../../config/database.json');
-        $config = json_decode($json)->develop;
+        // for auke.us
+        if (!getenv('DATABASE_URL')) {
+            $json = file_get_contents(__DIR__ . '/../../config/database.json');
+            $config = json_decode($json)->develop;
 
-        $link = new mysqli($config->host, $config->user, $config->password, $config->database);
+            $link = new mysqli($config->host, $config->user, $config->password, $config->database);
+        } else {
+            if (!preg_match('#mysql://([^:]*):([^@]*)@([^/]*)/(.*)#', strval(getenv('DATABASE_URL')), $matches)) {
+                die('mysql only');
+            }
+            $link = new mysqli($matches[3], $matches[1], $matches[2], 'user_matsu-yueh-390194');
+        }
         $link->set_charset('utf8');
         return new Pix_Table_Db_Adapter_Mysqli($link);
     }

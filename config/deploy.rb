4099af09afb7dcbddde6f05f2d6e1e84737a8fcf
beer <beer@eatgo.com>
2013-02-20 17:13:04 +0800

    default deploy production Signed-off-by: beer <beer@eatgo.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,8 @@ set :repository,  "set your repository location here"
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "/srv/www/#{application}-dev"
+#set :deploy_to, "/srv/www/#{application}-dev"
+set :deploy_to, "/srv/www/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:

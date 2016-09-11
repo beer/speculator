e50b2c384b72e9efdaa0000bb0df6f56e2992099
beer <beer@eatgo.com>
2016-09-11 17:55:25 +0800

    加上 tick modle Signed-off-by: beer <beer@eatgo.com>

---
+++ webdata/models/Tick.php
@@ -0,0 +1,31 @@
+<?php
+class TickRow extends Pix_Table_Row
+{
+    public function preInsert()
+    {
+        $this->created_at = time();
+    }
+
+    public function preSave()
+    {
+        $this->updated_at = time();
+    }
+}
+
+class Tick extends SpeculatorTable
+{
+    public function init()
+    {
+        $this->_name = 'ticks';
+        $this->_rowClass = 'TickRow';
+
+        $this->_primary = 'id';
+
+        $this->_columns['id'] = array('type' => 'int', 'size' => 10, 'unsigned' => true, 'auto_increment' => true);
+        $this->_columns['date'] = array('type' => 'int', 'size' => 10);
+        $this->_columns['time'] = array('type' => 'int', 'size' => 10);
+        $this->_columns['twse'] = array('type' => 'float', 'size' => 10);
+        $this->_columns['created_at'] = array('type' => 'int', 'size' => 10);
+        $this->_columns['updated_at'] = array('type' => 'int', 'size' => 10);
+    }
+}

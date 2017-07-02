cdd14a5463b02f9a14d38704c40f4ade6e157346
beer <beer@eatgo.com>
2017-07-03 02:13:45 +0800

    改用deployer 這個 user deploy code
    Signed-off-by: beer <beer@eatgo.com>

--- deploy.php
+++ deploy.php
@@ -6,7 +6,7 @@ require 'recipe/common.php';
 // Configuration
 
 set('repository', 'git@github.com:beer/speculator.git');
-set('git_tty', true); // [Optional] Allocate tty for git on first deployment
+//set('git_tty', true); // [Optional] Allocate tty for git on first deployment
 set('shared_files', ['config/database.json']);
 set('shared_dirs', []);
 set('writable_dirs', []);
@@ -16,23 +16,28 @@ set('default_stage', 'beta');
 // Hosts
 
 host('speculator.im')
+    ->user('deployer')
     ->stage('prod')
     ->set('deploy_path', '/srv/www/speculator');
     
 host('beta.speculator.im')
+    ->user('deployer')
     ->stage('beta')
     ->set('deploy_path', '/srv/www/beta.speculator');    
 
 
 // Tasks
 
+/*
+// 現在沒有用 php-fpm
 desc('Restart PHP-FPM service');
 task('php-fpm:restart', function () {
     // The user must have rights for restart service
     // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
     run('sudo systemctl restart php-fpm.service');
 });
-//after('deploy:symlink', 'php-fpm:restart');
+after('deploy:symlink', 'php-fpm:restart');
+*/
 
 desc('Deploy your project');
 task('deploy', [
@@ -42,7 +47,7 @@ task('deploy', [
     'deploy:update_code',
     'deploy:shared',
     'deploy:writable',
-    //'deploy:vendors',
+    //'deploy:vendors', // 還沒有用 composer
     'deploy:clear_paths',
     'deploy:symlink',
     'deploy:unlock',

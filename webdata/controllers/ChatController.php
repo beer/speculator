ffa1902c60e9ef6dcb84d37503c1aa14a6eda0c5
beer <beer@eatgo.com>
2017-08-03 18:40:25 +0800

    把圖型mv 到 chat controller & add todo list
    Signed-off-by: beer <beer@eatgo.com>

---
+++ webdata/controllers/ChatController.php
@@ -0,0 +1,40 @@
+<?php
+class ChatController extends Pix_Controller
+{
+    public function indexAction()
+    {
+
+    }
+
+    public function highstockAction()
+    {
+
+    }
+
+    public function moietyAction()
+    {
+
+    }
+
+    public function testAction()
+    {
+
+    }
+
+    public function twiiAction()
+    {
+        $candles = Candle::search(1);
+        $data = array();
+        foreach ($candles as $c) {
+            // UTC+8
+            $data[] = array($c->time * 1000 + 28800*1000, (float) $c->open, (float) $c->top, (float) $c->low, (float) $c->close);
+        }
+        $this->json($data);
+    }
+
+    public function infoAction()
+    {
+        $v = $this->view;
+        $v->day = strtotime($_GET['day']);
+    }
+}

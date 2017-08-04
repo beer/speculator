00e873b4b36bdeb1193abdd63576d294c0789689
beer <beer@eatgo.com>
2017-08-05 02:58:46 +0800

    add feed ajax sample
    Signed-off-by: beer <beer@eatgo.com>

--- webdata/controllers/AjaxController.php
+++ webdata/controllers/AjaxController.php
@@ -35,4 +35,72 @@ class AjaxController extends Pix_Controller
         }
         $this->json($data);
     }
+
+    public function feedAction()
+    {
+        $periodicity = 60; //秒為單位
+        $start = strtotime('2017-08-02');
+        $end = strtotime('2017-08-03');
+
+        $ticks = Tick::search("`date` >= {$start} AND `date` <= {$end}")->order('time ASC');
+        $length = count($ticks);
+
+        $data = array();
+
+        // 將tick 資料轉為 candle 
+        $row = array();
+        $pool = array();
+
+        $check_date = '';
+        // :DEBUG:
+        //$t = array();
+        
+        $i = 0;
+        foreach ($ticks as $tick) {
+            $i++;
+            // :DEBUG:
+            //$t[] = array(date('d-M-y H:i:s', $tick->time), (float) $tick->twse, $i);
+
+            if ($tick->time % $periodicity == 0) {
+                // 第一筆資料且資料長度不只一筆
+                if ($i == 1 and $length > 1) {
+                    $row['time'] = date('Y/m/d H:i:s', $tick->time);
+                    $row['open'] = $tick->twse;
+                    $pool[] = $tick->twse;
+                    continue;
+                }
+                // 結算週期資料，轉為candle, 並清空
+                if (count($pool)) {
+                    $row['top'] = max($pool);
+                    $row['low'] = min($pool);
+                    $row['close'] = array_pop($pool);
+                    $row['volume'] = rand(10, 1000);
+                    $data[] = array($row['time'], (float) $row['open'], (float) $row['top'], (float) $row['low'], (float) $row['close'], (float) $row['volume']);
+                    $pool = $row = array();
+                }
+                // 每週期的第一筆資料
+                $row['time'] = date('Y/m/d H:i:s', $tick->time);
+                $row['open'] = $tick->twse;
+            }
+            $pool[] = $tick->twse;
+            $check_date = $tick->date;
+
+            // 最後一筆資料，將pool 中的資料轉為candle
+            if ($i == $length or $check_date != $tick->date) {
+                $row['top'] = max($pool);
+                $row['low'] = min($pool);
+                $row['close'] = array_pop($pool);
+                $row['volume'] = rand(10, 1000);
+                $data[] = array($row['time'], (float) $row['open'], (float) $row['top'], (float) $row['low'], (float) $row['close'], (float) $row['volume']);
+                $pool = $row = array();
+            }
+        }
+        // :DEBUG:
+        //$data['ticks'] = $t;
+        $this->json($data);
+    }
+
+    public function tickAction()
+    {
+    }
 }
